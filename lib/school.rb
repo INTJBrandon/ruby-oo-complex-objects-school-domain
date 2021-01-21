c76b0d4701373e864d9be47b8afa0152db294178
seireitou <ausis@mail.com>
2021-01-21 13:57:12 -0600

    commit tree e2d82e52bc1e5982d119d7590972011b69fc34f1

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
+require 'pry'
 # code here!
+class School
+    attr_accessor :roster, :name
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+
+    def add_student(student, grade)
+        @roster[grade] ||= []
+        @roster[grade] << student
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+    
+    def sort
+        @roster = @roster.sort_by {|grade, student| grade }.to_h
+        i = 0
+        while i < @roster.values.length
+            @roster.values[i].sort!
+            i += 1
+        end
+        return @roster
+    end
+
+        
+end
